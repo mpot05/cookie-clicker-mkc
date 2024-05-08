0567b0c7b60a55c95c867f6267fda50925c0e409
Michael Potter <146374743+mpot05@users.noreply.github.com>
2024-05-08 08:13:17 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Cookie":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . e e e f e e e . . . . . 
+. . . e e e e e e e e e . . . . 
+. . . e e f e e e e e e . . . . 
+. . . e e e e e e f e e . . . . 
+. . . e e e f e e e e e . . . . 
+. . . e f e e e f e e e . . . . 
+. . . e e e e e e e e e . . . . 
+. . . e e e f e e e f e . . . . 
+. . . . e e e e e e e . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Pointer":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f . . . . . . . . . . 
+. . . . . f f . . . . . . . . . 
+. . . . . f 1 f . . . . . . . . 
+. . . . . f 1 1 f . . . . . . . 
+. . . . . f 1 1 1 f . . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . . f 1 1 1 1 1 f . . . . 
+. . . . . f 1 1 1 1 1 1 f . . . 
+. . . . . f 1 1 1 1 f f . . . . 
+. . . . . f 1 1 1 f . . . . . . 
+. . . . . f 1 f 1 1 f . . . . . 
+. . . . . f f . f 1 1 f . . . . 
+. . . . . f . . . f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "man1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "auto1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,73 @@
+namespace projectImages {
+    export const Cookie = assets.image`Cookie`;
+    export const Pointer = assets.image`Pointer`;
+    export const Man1 = assets.image`man1`;
+    export const Auto1 = assets.image`auto1`;
+}
+
+namespace SpriteKind {
+    export const Cookie = SpriteKind.create()
+    export const Man1 = SpriteKind.create()
+    export const Auto1 = SpriteKind.create()
+}
+scene.setBackgroundColor(9)
+
+let count = 0
+let auto = 0
+let clickpower = 1
+let man1Cost = 25
+let auto1Cost = 50
+
+let man1: Sprite = sprites.create(projectImages.Man1.doubled(), SpriteKind.Man1)
+man1.setPosition(40, 40)
+let auto1: Sprite = sprites.create(projectImages.Auto1.doubled(), SpriteKind.Auto1)
+auto1.setPosition(120, 40)
+
+let cookie: Sprite = sprites.create(projectImages.Cookie, SpriteKind.Cookie)
+let pointer: Sprite = sprites.create(projectImages.Pointer, SpriteKind.Player)
+pointer.changeScale(0.00000000000000000000000001, ScaleAnchor.Middle)
+cookie.changeScale(1,ScaleAnchor.Middle)
+pointer.setStayInScreen(true)
+controller.moveSprite(pointer)
+info.showScore(true)
+
+
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
+    if (pointer.overlapsWith(cookie)) {
+        count += clickpower
+        info.setScore(count)
+    }
+    else if (pointer.overlapsWith(man1)) {
+        if (count >= man1Cost) {
+            count -= man1Cost
+            man1Cost *= 1.25
+            clickpower += 1
+            info.setScore(count)
+        }
+        else {
+            man1.sayText("Cost:"+Math.round(man1Cost),1000)
+        }
+    }
+    else if (pointer.overlapsWith(auto1)) {
+        if (count >= auto1Cost) {
+            count -= auto1Cost
+            auto1Cost *= 1.25
+            auto += 1
+            info.setScore(count)
+        }
+        else {
+            auto1.sayText("Cost:" + Math.round(auto1Cost), 1000)
+        }
+    }
+
+})
+
+game.onUpdateInterval(500, () => {
+    info.setScore(count)
+})
+
+game.onUpdateInterval(1000, () => {
+    count += auto
+    info.setScore(count)
+})
